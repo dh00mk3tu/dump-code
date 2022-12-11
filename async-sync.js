aa728a7e3ff4777169cc6cdb324586cf7ac49e91
dh00mk3tu <38985076+dh00mk3tu@users.noreply.github.com>
2022-12-11 19:11:35 +0530

    sync

---
+++ async-sync.js
@@ -0,0 +1,26 @@
+let isLoading = false;
+const test = async () => { /// it is taking 10s 
+    await Promise.all;
+}
+
+
+const AppBar =() => {
+
+}
+if(isLoading) {
+    // loading     
+}
+const test2 = () => { // takes 60s 
+    console.log("hi AGAIN");
+}
+
+const test3 = () => {
+    await console.log("hihihi");
+}
+
+const test4 = () => {
+    console.log("hi fourth time");
+}
+
+// test() // execute - we are awaiting the result 60s-70s 
+
